919883a1a12bcffa5f12a23b481846dffc0eb675
hmj <hongseluv@gmail.com>
2023-04-10 18:24:06 +0900

    Revert "[스프링부트-게시판]"
    This reverts commit 5e09211482dae7faadf5abb62b756c76ccf5d118.

--- src/main/java/com/study/board/entity/Board.java
+++
@@ -1,7 +0,0 @@
-package com.study.board.entity;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Board {
-}
--- src/main/java/com/study/board/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.study.board.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class BoardRepository {
-}
