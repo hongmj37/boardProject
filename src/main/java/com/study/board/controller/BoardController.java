5e09211482dae7faadf5abb62b756c76ccf5d118
hmj <hongseluv@gmail.com>
2023-04-10 18:22:55 +0900

    [스프링부트-게시판]

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -2,15 +2,23 @@ package com.study.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
 
-    @GetMapping("/") //\
-    @ResponseBody
-    public String main() {
-        return "Hello Spring !!";
-        //GetMapping의 주소로 들어왔을 때, ResponseBody로 return에 해당하는 글자를 띄워주는 역할을 함 !
+    @GetMapping("/board/write") //localhost:8080/board/write에 접속하면 boardWrite.html에 접속하겠다는 의미
+    public String boardWriteForm() {
+
+        return "boardwrite";
     }
+
+    @PostMapping("/board/writePro")
+    public String boardWritePro(String title, String content) { //매개변수로 String형의 title과 content를 받는다는 의미
+        System.out.println("제목 : " + title);
+        System.out.println("내용 : " + content);
+        return "";
+    }
+
 }
---
+++ src/main/java/com/study/board/repository/BoardRepository.java
@@ -0,0 +1,7 @@
+package com.study.board.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BoardRepository {
+}
